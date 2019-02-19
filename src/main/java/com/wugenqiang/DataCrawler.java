f74a292bcced53cf0318aa015ab75d79e19ba928
wugenqiang <2422676183@qq.com>
2019-02-19 17:54:04 +0800

    文章详情页爬虫

--- src/main/java/com/wugenqiang/DataCrawler.java
+++ src/main/java/com/wugenqiang/DataCrawler.java
@@ -20,7 +20,9 @@ import java.io.IOException;
  * @Created by WuGenQiang//fighting!!
  */
 public class DataCrawler {
+
     public static void main(String[] args) {
+
         String url = "https://blog.csdn.net/wugenqiang/article/list/2";
         String rawHTML = null;
         try {
@@ -28,22 +30,23 @@ public class DataCrawler {
         } catch (IOException e) {
             e.printStackTrace();
         }
-//将当前页面转换成Jsoup的Document对象
+        //将当前页面转换成Jsoup的Document对象
         Document doc = Jsoup.parse(rawHTML);
-//获取所有的博客列表集合
+        //获取所有的博客列表集合
         Elements blogList = doc.select("div[data-articleId]");
 //针对每个博客内容进行解析，并输出
         for (Element element : blogList) {
-            String title = element.select("h4[class]").text().substring(2);
-            String content = element.select("p[class=content]").text();
-
-            System.out.println("Title:\t" + title);
-            System.out.println("introduction:\t" + content);
-            System.out.println("--------------------------");
+            //String title = element.select("h4[class]").text().substring(2);
+            String url1 = element.getElementsByTag("a").get(0).attr("href");
+            getContentUrl(url1);
         }
+
+
+
     }
+
     //根据url地址获取对应页面的HTML内容，我们将上一节中的内容打包成了一个方法，方便调用
-    private static String getHTMLContent( String url ) throws IOException {
+    private static String getHTMLContent(String url) throws IOException {
 //建立一个新的请求客户端
         CloseableHttpClient httpClient = HttpClients.createDefault();
 //使用HttpGet方式请求网址
@@ -56,5 +59,27 @@ public class DataCrawler {
 //关闭HttpEntity流
         EntityUtils.consume(entity);
         return content;
+    }
+
+
+    private static void getContentUrl(String url) {
+
+        //String url = "https://blog.csdn.net/wugenqiang/article/details/86513465";
+        String rawHTML = null;
+        try {
+            rawHTML = getHTMLContent(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //将当前页面转换成Jsoup的Document对象
+        Document doc = Jsoup.parse(rawHTML);
+        //获取所有的博客列表集合
+        Elements blogList1 = doc.select("div[class=blog-content-box]");
+        for (Element element1 : blogList1) {
+            String title = element1.select("h1[class=title-article]").text();
+            String content = element1.select("div[class=htmledit_views]").text();
+            System.out.println("Title:\t" + title);
+            System.out.println("Content:\t" + content);
+        }
     }
 }
